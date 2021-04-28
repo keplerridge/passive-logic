c6307b9dc069ac6aa4efa5fe97df673c043ec28c
keplerridge <kepler.ridge@gmail.com>
2021-04-28 14:22:49 -0600

    finished logic, going to switch this from a react app to a basic js app

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const App = () => {
 
   useEffect(() => {
     if(tankTemp >= 120){
-      setPump(false)
+      setInterval(stagnant(tankTemp), 6000)
     } else if (tankTemp <= 115){
-      pumping(tankTemp)
+      setInterval(pumping(tankTemp), 6000)
     }
   }, [pump])
 
@@ -19,32 +19,40 @@ const App = () => {
 // Temperature equation for incoming water
 // T(final) = (m1 * T1 + m2 * T2) / (m1 + m2)
 
-  const pumping = (tankTemp) => {
+  const increasing = () => {
     let t1 = tankTemp
+    let m2 = gpm / 10
     let m1 = tankMass - m2
     let t2 = tankTemp + 2
-    let m2 = gpm / 10
+
+    do {
+      let T = (m1 * t1 + m2 * t2) / (m1 + m2)
+      setTankTemp(T)
+    } while (tankTemp < 120)
+  }
+
+  const pumping = (tankTemp) => {
 
     if(tankTemp <= 115){
-      do {
-        let T = (m1 * t1 + m2 * t2) / (m1 + m2)
-        setTankTemp(T)
-      } while (tankTemp < 120)
+      increasing()
     } else {
       setPump(false)
     }
   }
 
 // Quick google search said a well insulated water heater loses about 1/2 degree to 1 degree of temperature per hour
-// I am using that to calculate the heat loss at 1 degree per hour
-// 1 / 60 / 10
+// I am using that to calculate the heat loss at 1 degree per hour 1 / 60 / 10
+
+  const decreasing = () => {
+    do {
+      setTankTemp(tankTemp - (1 / 60 / 10))
+    } while (tankTemp >= 120)
+  }
 
   const stagnant = (tankTemp) => {
 
     if(tankTemp >= 120){
-      do {
-        setTankTemp(tankTemp - (1 / 60 / 10))
-      } while (tankTemp >= 120)
+      decreasing()
     } else {
       setPump(true)
     }
@@ -53,7 +61,7 @@ const App = () => {
   return (
     <div className="App">
       <button onClick={setPump(true)}>Start Simulation</button>
-      <div>{console.log(tankTemp)} degrees<br></br></div>
+      <div>{console.log(tankTemp)}</div>
     </div>
   );
 }
